90b17c09c442731eacb4ba934fe8420a8966d11c
Alexandra Guranda <161466046+alexandra-guranda@users.noreply.github.com>
2025-03-26 16:55:08 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddControllersWithViews();
+builder.Services.AddSession(); // Adaugă suport pentru sesiuni
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowReactApp", builder =>
+    {
+        builder.WithOrigins("http://localhost:3000") // Adaugă URL-ul frontend-ului
+               .AllowAnyHeader()
+               .AllowAnyMethod();
+    });
+});
+
+var app = builder.Build(); // Inițializează aplicația
+
+// Configure the HTTP request pipeline.
+if (!app.Environment.IsDevelopment())
+{
+    app.UseExceptionHandler("/Home/Error");
+    app.UseHsts();
+}
+
+app.UseHttpsRedirection();
+app.UseRouting();
+app.UseCors("AllowReactApp"); // Activează politica CORS
+app.UseSession(); // Activează sesiunea
+app.UseAuthorization();
+
+app.MapStaticAssets();
+
+app.MapControllerRoute(
+    name: "default",
+    pattern: "{controller=Home}/{action=Index}/{id?}")
+    .WithStaticAssets();
+
+app.Run();
